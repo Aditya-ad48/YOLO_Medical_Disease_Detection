65f1b443f756508847d7f1e40d3611927ace4a76
Aditya-ad48 <adityaraj13.ahirwar@gmail.com>
2025-04-05 16:40:19 +0530

    Update app1.py

--- app1.py
+++ app1.py
@@ -22,10 +22,11 @@ model = YOLO(MODEL_PATH)
 st.title("🚀 YOLOv8 Object Detection Web App")
 uploaded_file = st.file_uploader("Upload an image", type=["jpg", "jpeg", "png"])
 
+
 if uploaded_file:
     image = Image.open(uploaded_file)
-    st.image(image, caption="Uploaded Image", use_column_width=True)
+    st.image(image, caption="Uploaded Image", use_container_width=True)
     with st.spinner("Detecting..."):
         results = model.predict(image)
     result_img = results[0].plot()
-    st.image(result_img, caption="Detected Image", use_column_width=True)
+    st.image(result_img, caption="Detected Image", use_container_width=True)
